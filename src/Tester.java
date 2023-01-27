446a2069bd75a1c1ec11ffcf973e7b042ba4bfc7
Eduard <eduard.prog22@gmail.com>
2023-01-27 14:58:27 +0100

    First try

---
+++ src/Tester.java
@@ -0,0 +1,15 @@
+public class Tester {
+    public Tester(){
+    }
+    public static void main(String[] args){
+
+        String string = "I Like to code near Lake with my friend Luke";
+
+        System.out.println(string.replaceAll("L[^u]ke","BLA."));
+
+
+
+
+
+    }
+}
